f34f8911f6ad9a8ef7e236f3bc56fbabbeb80c5a
ahyun <hong82679944@gmail.com>
2023-12-24 21:17:31 +0900

    게시판 조회,수정,삭제,추가(더미데이터)기능

---
+++ src/pages/QnAPage/Section/QnADetail/styled.jsx
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    .QnADetail {
+        // box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        // border: 1px solid #ddd;
+        text-align: center;
+        padding : 20px;
+        margin-left: 265px;
+        
+    }
+
+    .detail {
+        margin-bottom: 20px;
+        width: 500px;
+        pdding: 10px;
+        height: 30px;
+    }
+
+    .QnADetail textarea {
+        height: 200px;
+    }
+
+    .QnADetail button {
+        width: 20%;
+        padding: 10px;
+        cursor: pointer;
+        background: #99CCFF; 
+        color: #fff;
+        border-radius:5px;
+        border-color: #99CCFF;
+    }
+
+    .btn-container {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .left-btn {
+        margin-right: auto; 
+    }
+      
+    .right-btns {
+        display: flex;
+    }
+      
+    .right-btns button {
+        margin-left: 10px;
+        white-space: nowrap;
+        width:42%;
+    }
+
+    .QnADetail button:hover,
+    .QnADetail button:focus {
+        background: #0000FF; 
+        border-color: #0000FF; 
+        color: #fff;
+    }
+
+    .QnADetail div p {
+        margin: 0;
+        // margin-left: 0;
+        text-align: left;
+      }
+`;
--- src/pages/QnAPage/index.jsx
+++ src/pages/QnAPage/index.jsx
@@ -1,11 +1,95 @@
+import { useContext, useState} from "react";
+import { useNavigate } from "react-router-dom";
 import { Container } from "./styled";
+import { FaSearch } from "react-icons/fa";
+import { PostStateContext } from "../../App";
+//import Pagination from "react-js-pagination";
 
 const QnAPage = () => {
+
+  const postList = useContext(PostStateContext);
+
+    // const [currentPage, setCurrentPage] = useState(1);
+
+    // const handlePageChange = (page) => {
+    //     setCurrentPage(page);
+    // }
+    const navigate = useNavigate();
+
+    const handleWriteButtonClick = () => {
+        navigate('/question/create');
+    };
+    // console.log(postList);
+    
+
   return (
     <Container>
-      <h1>QnAPage</h1>
+    
+    <section class="notice">
+        <div class="page-title">
+            <div class="container">
+                <h3>질문게시판</h3>
+            </div>
+        </div>
+
+        <div id="board-search">
+                <div class="container">
+                    
+                <div className="write-wrap">
+                    <button class="btn write-btn" onClick={handleWriteButtonClick}>글쓰기</button>
+                </div>
+                    <div class="search-window">
+                        <form action="">
+                            <div class="search-wrap">
+                                <label for="search" class="blind">질문게시판 내용 검색</label>
+                                <input id="search" type="search" name="" placeholder="검색어를 입력해주세요." value=""/>
+                                <button type="submit" class="btn search-btn">검색 <FaSearch className="search-icon"/></button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div id="board-list">
+                <div class="container">
+                    <table class="board-table">
+                        <thead>
+                        <tr>
+                            <th scope="col" class="th-num">번호</th>
+                            <th scope="col" class="th-title">제목</th>
+                            <th scope="col" class="th-date">등록일</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {postList.map((it)=> (
+                            <tr key={it.id}>
+                                <td>{it.id}</td>
+                                <th><a href={`/question/${it.id}`}>{it.title}</a></th>
+                                <td>{new Date(it.date).toLocaleString()}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
+        </div>
+
+    </section>
+    <div style={{ marginTop: '-40px' }}>
+    {/* <Pagination
+      activePage={currentPage}
+      itemsCountPerPage={5}
+      totalItemsCount={5}
+      pageRangeDisplayed={5}
+      prevPageText={"‹"}
+      nextPageText={"›"}
+      onChange={handlePageChange}
+    /> */}
+    </div>
     </Container>
   );
+};
+
+QnAPage.defaultProps = {
+    diaryList:[],
 };
 
 export default QnAPage;
