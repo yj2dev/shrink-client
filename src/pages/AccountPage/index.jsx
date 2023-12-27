64623642675562959c54dee4c3eb423a6bff7303
ahyun <hong82679944@gmail.com>
2023-12-27 16:44:08 +0900

    계정정보 페이지 초안

--- src/pages/AccountPage/index.jsx
+++ src/pages/AccountPage/index.jsx
@@ -1,9 +1,118 @@
+import { useState } from "react";
 import { Container } from "./styled";
+import ProfileImgModal from "../../components/ProfileImgModal";
+import { GoPencil } from "react-icons/go";
 
 const AccountPage = () => {
+
+  const [showMenu, setShowMenu] = useState(false);
+  const [nickname, setNickname] = useState("가리비공주");
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [showProfileImgModal, setShowProfileImgModal] = useState(false);
+  const [previewImage, setPreviewImage] = useState(null);
+
+  const handleFileSelect = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setSelectedFile(file);
+    }
+  };
+
+  const handleFileUpload = () => {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      setPreviewImage(event.target.result);
+    };
+    reader.readAsDataURL(selectedFile);
+
+    setShowProfileImgModal(false);
+  }
+
+  const onShowProfileImgModal = () => {
+    setShowProfileImgModal(true);
+  }
+
+  const onCloseModal = () => {
+    setShowProfileImgModal(false);
+  };
+
+  const handleNameInputChange = (e) => {
+    setNickname(e.target.value);
+  };
+
   return (
     <Container>
-      <h1>AccountPage</h1>
+      <div className="account-wrap">
+        <div className="account-info">
+        <section className="account-total">
+          <section className="account-first">
+        <h2>내 계정정보</h2>
+        </section>
+        <div className="profile-img-wrapper">
+          <p>프로필 이미지</p>
+        {previewImage ? (
+          <img
+            src={previewImage}
+            alt="미리 보기"
+            style={{ maxWidth: '100%', marginTop: '20px' }}
+        />
+      ) : (
+          <img src="https://picpac.kr/common/img/default_profile.png" alt="basic-img"/>
+      )}
+          
+          
+          <button class='menu-btn' onClick={() => setShowMenu(!showMenu)}><GoPencil className="pencil"/>편집</button>  
+            {showMenu && (
+              <div class='menu'>  
+              <div class='menu-msg'>
+              
+              <button 
+              className="img-edit"
+              onClick={onShowProfileImgModal}
+              >
+                프로필 이미지 변경
+                </button>
+              <button className="img-reset"
+              onClick={()=> {
+                setPreviewImage(null);
+              }}
+              >
+                프로필 초기화
+                </button>
+                </div>
+              </div>
+            )}
+            
+         
+        </div>
+        </section>
+        <div className="profile-descript">
+            <p>닉네임</p>
+            <input 
+            className="name-descript" 
+            value={nickname}
+            onChange={handleNameInputChange}
+            />
+            <p>휴대폰번호</p>
+            <input 
+            className="name-descript" 
+            value="01012345678" 
+            readOnly/> 
+
+            <button className="mod-btn">수정하기</button>
+          </div>
+        
+        </div>
+        
+      </div>
+
+      <ProfileImgModal 
+      onHandleFile={handleFileSelect}
+      onUploadFile={handleFileUpload}
+      show={showProfileImgModal}
+      onClose={onCloseModal}
+      />
+      
     </Container>
   );
 };
--- src/pages/AccountPage/styled.jsx
+++ src/pages/AccountPage/styled.jsx
@@ -1,2 +1,181 @@
 import styled from "styled-components";
-export const Container = styled.div``;
+export const Container = styled.div`
+
+width: 80%;
+.account-wrap {
+  display: flex;
+  justify-content: center;
+  min-height: 100vh; 
+  align-items: center;
+  margin: 0px;
+}
+
+@media (min-width: 650px) {
+    .account-info {
+        width:640px;
+    }
+}
+
+@media (max-width: 650px) {
+    .account-info {
+        width:90vw;
+    }
+}
+
+.account-info {
+    background-color: white;
+    box-shadow: rgba(100,100,111,0.2) 0px 7px 29px 0px;
+    min-height: 100vh;
+    padding-left: 20px;
+    padding-right: 20px;
+    margin-top: 20px;
+    border-radius: 15px;
+
+}
+
+img {
+    width: 80%;
+}
+
+
+.account-total {
+    width: 100%;
+    height: 100%;
+    // margin-bottom: 100px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+  
+
+}
+
+.account-first {
+    width: 80%;
+    // display: flex;
+    // flex-direction: column;
+    // align-items: center;
+    // text-align: center;
+    border-bottom: 1px solid #ccc;
+}
+
+.profile-img-wrapper {
+    // border-top: 1px solid #ccc;
+    padding-top: 20px;
+    position: relative;
+    max-width: 250px;
+    width: 250px;
+}
+
+
+.profile-img-wrapper input {
+    display: none;
+}
+
+img {
+    border-radius:50%;
+}
+
+.menu-btn {
+    position:absolute;
+    right: 15px;
+    bottom: 10px;
+    background-color:white;
+    border: 1px solid;
+    border-radius: 5px;
+    cursor: pointer;
+}
+
+.menu {
+    width:170px;
+    position:absolute;  
+    right: -153px;
+    bottom: -40px;
+
+    flex-shrink: 1;
+    height: 115px;
+    //width: 300px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-right: 5%;
+    
+}
+.menu::before {
+    content: "";
+    top: 50%;
+    left: 1em;
+    border: 1em solid transparent;
+    border-right: 1em solid #F2F2F2;
+    z-index: 0;
+  }
+
+
+.menu-msg {
+    display: flex;
+    position: relative;
+    width: 100%;
+    height: 60%;
+    justify-content: center;
+    align-items: center;
+    background: #F2F2F2;
+    border-radius: 15px;
+    // padding-left: 1.8em;
+    // padding-right: 1.8em;
+    flex-direction: column;
+    font-size:13px;
+  }
+
+  .name-descript {
+    background-color: #F2F2F2;
+    border-radius:5px;
+    height: 40px;
+    width: 250px;
+    margin-left: 20px;
+    border: none;
+    padding-left: 20px;
+  }
+
+  p {
+    //text-align: left;
+    //margin-left: 20px;
+    font-size: 15px;
+    margin-bottom: 15px;
+  }
+
+  .profile-descript {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+  }
+
+  .img-edit, .img-reset {
+    border: none;
+    backgroud: rgba(245,243,245, 0.9);
+    cursor: pointer; 
+  }
+
+  .pencil {
+    font-size: 12px;
+  }
+
+  .mod-btn {
+    border: none;
+    margin-top: 30px;
+    font-size: 15px;
+    padding-left: 10px;
+    padding-right: 10px;
+    padding-top: 5px;
+    padding-bottom: 5px;
+
+    background-color: #99CCFF;
+    color: white;
+
+    cursor: pointer;
+  
+  }
+
+`;
