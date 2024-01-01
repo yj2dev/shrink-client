8562c3a710aa77c6121f54481dcce52f56f3f7cb
leeyujin <nogo22@naver.com>
2024-01-01 20:54:57 +0900

    update: 로고 변경, 로고 호버 액션 추가

--- src/layouts/Header/styled.jsx
+++ src/layouts/Header/styled.jsx
@@ -26,6 +26,7 @@ export const Container = styled.header`
   z-index: 220;
   width: 100%;
   padding: 16px 30px;
+  height: 78px;
   box-sizing: border-box;
   box-shadow: 0 4px 4px rgba(0, 0, 0, 0.1);
   background-color: rgba(255, 255, 255, 0.7);
@@ -42,16 +43,45 @@ export const LeftSection = styled.div`
   }
 
   .logo-img {
-    width: 32px;
-    height: 32px;
+    //width: 32px;
+    //height: 32px
+    height: 78px;
     margin-right: 10px;
   }
 
-  h1 {
-    font-size: 2rem;
-    font-weight: bold;
-    padding: 0;
-    margin: 0;
+  .content {
+    overflow: hidden;
+
+    h1 {
+      white-space: nowrap;
+      //border: 1px solid red;
+      width: 185px;
+      font-weight: bold;
+      //font-size: 2rem;
+      padding: 0;
+      margin: 0;
+      transition: 0.5s ease all;
+    }
+
+    h1.active {
+      font-size: 1.52rem;
+      letter-spacing: 12px;
+    }
+
+    h3 {
+      margin: 0;
+      padding: 0;
+      font-size: 0.75rem;
+      height: 0;
+      transition: 0.5s ease all;
+    }
+
+    h3.active {
+      margin: 0;
+      padding: 0;
+      font-size: 0.75rem;
+      height: 16px;
+    }
   }
 
   @media (max-width: 768px) {
