b30022ff7bc709794ec85a3af8c8365b43eb8c76
leeyujin <nogo22@naver.com>
2024-01-01 23:56:34 +0900

    update: favicon 적용

--- src/pages/ReportPage/index.jsx
+++ src/pages/ReportPage/index.jsx
@@ -1,8 +1,55 @@
 import { Container } from "./styled";
+import axios from "axios";
+import { useEffect } from "react";
 
 const ReportPage = () => {
+  useEffect(() => {
+    getReportList();
+  }, []);
+
+  const getReportList = () => {
+    axios
+      .get("/api/report/selectall")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const onClickCreateReport = () => {
+    const fd = new FormData();
+
+    let payload = {
+      product: "1004",
+      weight: "900",
+      price: "1500",
+      content: "배고프다",
+    };
+
+    payload = `{
+        "product":"2221004",
+        "weight":"900",
+        "price": 2000,
+        "content":"파인애플맛 뺴뺴로 누드빼빼로"
+    }`;
+
+    fd.append("data", payload);
+
+    axios
+      .post("/api/report/create", fd)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <Container>
+      <button onClick={onClickCreateReport}>글쓰기</button>
       <h1>ReportPage</h1>
     </Container>
   );
--- src/pages/ReportPage/styled.jsx
+++ src/pages/ReportPage/styled.jsx
@@ -1,3 +1,11 @@
 import styled from "styled-components";
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 32px;
+
+  //width: 100%;
+  //justify-content: center;
+  //align-items: center;
+  //position: relative;
 `;
