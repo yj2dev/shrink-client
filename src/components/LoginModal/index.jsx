9ae737bf745157343a817e1fa9ea87fa3d8f54e5
SeungJu3 <crongsj99@gmail.com>
2024-01-04 13:50:19 +0900

    fix: loginModal 경로 수정

--- src/components/LoginModal/index.jsx
+++ src/components/LoginModal/index.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSetRecoilState } from "recoil";
-import { userState } from "../../state/selectors/userSelectors";
+import { userState } from "../../state/userState";
 import Modal from "../Modal";
 import "./index.css";
 
