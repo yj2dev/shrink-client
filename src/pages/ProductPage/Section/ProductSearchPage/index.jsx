3d5c7c5ae50e95a098c0d5a4e59a8b43182f0515
leeyujin <nogo22@naver.com>
2024-01-07 07:38:38 +0900

    design: 상품 결과 페이지 디자인

--- src/pages/ProductPage/Section/ProductSearchPage/index.jsx
+++ src/pages/ProductPage/Section/ProductSearchPage/index.jsx
@@ -1,22 +1,17 @@
-import { Container } from "./styled";
+// import { Container } from "./styled";
 import { useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
 import ProductPage from "../..";
 
-
 const ProductSearchPage = () => {
   const location = useLocation();
   const [keyword, setKeyword] = useState(location.state.keyword);
-  
+
   useEffect(() => {
     setKeyword(location.state?.keyword);
   }, [location]);
-  
-  return (
-    <Container>
-      <ProductPage keyword={keyword}/>
-    </Container>
-  );
+
+  return <ProductPage keyword={keyword} />;
 };
 
 export default ProductSearchPage;
---
+++ src/pages/ProductPage/old/index.jsx
@@ -0,0 +1,83 @@
+// import { useEffect, useState } from "react";
+// import { Container } from "./styled";
+// import axios from "axios";
+// import { FiAlertCircle } from "react-icons/fi";
+// import { useNavigate } from "react-router-dom";
+//
+//
+// const ProductPage = ({keyword}) => {
+//
+//   const [product, setProduct] = useState([]);
+//   const navigate = useNavigate();
+//
+//   const fetchProductData = async () => {
+//     try {
+//       const response = await axios.get("/api/product/selectall");
+//       const responseData = response.data;
+//       if(keyword){
+//         const filtered = responseData.response.filter((p) =>
+//           p.product_name.toLowerCase().includes(keyword.toLowerCase()),
+//         );
+//         setProduct(filtered);
+//       }else {
+//         setProduct(responseData.response);
+//       }
+//     } catch (error) {
+//       console.error("Error fetching product:", error.message);
+//     }
+//   };
+//
+//   useEffect(() => {
+//     fetchProductData();
+//   }, [keyword]);
+//
+//   useEffect(() => {
+//     fetchProductData();
+//   }, []);
+//
+//   //console.log("p",product);
+//   // console.log("k",keyword);
+//
+//   return (
+//     <Container>
+//       {keyword && product.length === 0 ? (
+//         <div className="nosearch-wrap">
+//           <FiAlertCircle className="nosearch"/>
+//           <h1>"{keyword}"에 대한 검색 결과가 없습니다.</h1>
+//           <button
+//             onClick={() => {
+//               navigate(-1);
+//             }}>
+//             이전 페이지로 돌아가기
+//           </button>
+//         </div>
+//       ) : (
+//         <div className="wrapper">
+//         <div className="products">
+//           {product.map((it) => (
+//             <div className="product" key={it.product_id}>
+//             <div className="product_img">
+//                   <img src={it.image ? it.image : "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbnOSHZ%2FbtrLTB8V5DQ%2FnlaUCKg7kzbp7PbVKy63Qk%2Fimg.png"} alt="product-img"/>
+//             </div>
+//             <div className="product_details">
+//               <h3>{it.product_name}</h3>
+//               <div className="detail">{it.detail ? it.detail : ""}</div>
+//               <div className="price">
+//                 <div className="price_l">
+//                   {/* <span className="weight_label">{it.weight}g</span> */}
+//                 </div>
+//               </div>
+//             </div>
+//             <div className="shrink-notice">꼬리표</div>
+//       </div>
+//       ))}
+//
+//     </div>
+//     </div>
+//       )}
+//
+//     </Container>
+//   );
+// };
+//
+// export default ProductPage;
