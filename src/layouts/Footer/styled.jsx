f46eb17457cc35053eed66d8e6debf0a14f0b5e9
Dain <ekdls010114@gmail.com>
2024-01-09 15:08:35 +0900

    footer 좀 더 내림

--- src/layouts/Footer/styled.jsx
+++ src/layouts/Footer/styled.jsx
@@ -3,12 +3,12 @@ export const Container = styled.footer`
     .nav-link {
         .contentButton {
             margin-left: 12%;
-            margin-top: 1%;
+            margin-top: 5%;
             margin-bottom: 1%;
             margin-right: 5%;
-            font-size: 16px;
+            font-size: 15px;
             border: none;
-            font-weight: 800;
+            font-weight: 650;
             color: black;
             background-color: white;
             // text-decoration: underline;
